19e8ce4ce71e539dedcfd8ba039cda795dab1d4a
jarbelaezestrada <julianaarbelaezestrada@gmail.com>
2023-12-12 16:19:04 -0500

    Implementacion de REST API

--- server.js
+++ server.js
@@ -2,30 +2,128 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv").config();
 const app = express();
+const bodyParser = require("body-parser");
+const fs = require("fs");
 const port = 8080;
 const listViewRouter = require("./list-view-router");
 const listEditRouter = require("./list-edit-router");
 
 app.use(express.json());
+app.use(bodyParser.json());
 
-const autMiddleware = ((req ,res , next) => {
+app.get("/Tareas", (req, res) => {
+  try {
+    const data = fs.readFileSync("tareas.json", "utf-8");
+    const parsedData = JSON.parse(data);
+    res.json(parsedData);
+  } catch (error) {
+    res.status(500).json({ error: "Error al leer el archivo de tareas" });
+  }
+});
 
-    const heaterToken = req.headers.autorizacion;
-    if (!heaterToken) {
-      return res.status(400).send({ error: "No existe el token" });
+app.post("/Tareas", (req, res) => {
+  try {
+    // Lee las tareas existentes del archivo
+    const data = fs.readFileSync("tareas.json", "utf-8");
+    const tareas = JSON.parse(data);
+
+    // Obtén la nueva tarea desde el cuerpo de la solicitud
+    const nuevaTarea = req.body.tareas;
+
+    // Agrega la nueva tarea al arreglo de tareas
+    tareas.push(nuevaTarea);
+
+    // Guarda las tareas actualizadas en el archivo
+    fs.writeFileSync("tareas.json", JSON.stringify(tareas, null, 2), "utf-8");
+
+    res.status(201).send({ mensaje: "Tarea creada con éxito" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: "Error al crear la tarea" });
+  }
+});
+
+app.put("/Tareas/:id", (req, res) => {
+  try {
+    // Lee las tareas existentes del archivo
+    const data = fs.readFileSync("tareas.json", "utf-8");
+    const tareas = JSON.parse(data);
+
+    // Obtén el ID de la tarea a actualizar desde los parámetros de la URL
+    const tareaId = req.params.id;
+
+    // Encuentra la tarea con el ID correspondiente en el arreglo de tareas
+    const tareaIndex = tareas.findIndex((tarea) => tarea.id === tareaId);
+
+    if (tareaIndex === -1) {
+      // Si no se encuentra la tarea, responde con un código 404 (No encontrado)
+      return res.status(404).send({ mensaje: "Tarea no encontrada" });
     }
 
-    jwt.verify(heaterToken, process.env.SECRETKEY, (err, decoded)=>{
-      if (err){
-        res.status(400).send({ error: "Token invalido"});
-      }
-    });
+    // Actualiza la tarea con los nuevos datos del cuerpo de la solicitud
+    tareas[tareaIndex] = { ...tareas[tareaIndex], ...req.body };
 
-    //req.rol = decoded;
-    next();
-  
+    // Guarda las tareas actualizadas en el archivo
+    fs.writeFileSync("tareas.json", JSON.stringify(tareas, null, 2), "utf-8");
+
+    res.status(200).send({ mensaje: "Tarea actualizada con éxito" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: "Error al actualizar la tarea" });
+  }
+});
+
+app.delete("/Tareas/:id", (req, res) => {
+  try {
+    // Lee las tareas existentes del archivo
+    const data = fs.readFileSync("tareas.json", "utf-8");
+    const tareas = JSON.parse(data);
+
+    // Obtén el ID de la tarea a eliminar desde los parámetros de la URL
+    const tareaId = req.params.id;
+
+    // Filtra las tareas para excluir la tarea con el ID correspondiente
+    const tareasActualizadas = tareas.filter((tarea) => tarea.id !== tareaId);
+
+    // Verifica si alguna tarea fue eliminada
+    if (tareasActualizadas.length === tareas.length) {
+      // Si no se encuentra la tarea, responde con un código 404 (No encontrado)
+      return res.status(404).send({ mensaje: "Tarea no encontrada" });
+    }
+
+    // Guarda las tareas actualizadas en el archivo
+    fs.writeFileSync(
+      "tareas.json",
+      JSON.stringify(tareasActualizadas, null, 2),
+      "utf-8"
+    );
+
+    res.status(200).send({ mensaje: "Tarea eliminada con éxito" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: "Error al eliminar la tarea" });
+  }
+});
+
+const writeTasksToFile = (tasks) => {
+  fs.writeFileSync("tasks.json", JSON.stringify(tasks, null, 2), "utf-8");
+};
+
+const autMiddleware = (req, res, next) => {
+  const heaterToken = req.headers.autorizacion;
+  if (!heaterToken) {
+    return res.status(400).send({ error: "No existe el token" });
+  }
+
+  jwt.verify(heaterToken, process.env.SECRETKEY, (err, decoded) => {
+    if (err) {
+      res.status(400).send({ error: "Token invalido" });
+    }
   });
 
+  //req.rol = decoded;
+  next();
+};
 
 const users = [
   {
